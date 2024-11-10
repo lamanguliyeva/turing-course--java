f1e5ca9f6e9eb6267d979897a12b97fc2699c50b
Laman400 <lmanquliyeva70@gmail.com>
2024-11-10 16:17:53 +0400

    initial commit

--- src/palindrome/Main.java
+++ src/palindrome/Main.java
@@ -1,4 +1,22 @@
 package palindrome;
+import java.util.Scanner;
 
 public class Main {
+    public static boolean isPalindrome(int x) {
+        int a = x;
+        int b = 0;
+        while (x > 0) {
+            b = b* 10 + x % 10;
+            x /= 10;
+        }
+        return a == b;
+    }
+
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int a = sc.nextInt();
+        Main solution = new Main();
+        System.out.println(solution.isPalindrome(a));
+    }
 }
--- src/smallestEvenMultiple/Solution.java
+++ src/smallestEvenMultiple/Solution.java
@@ -1,4 +1,17 @@
 package smallestEvenMultiple;
 
+import java.util.Scanner;
+
 public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int n = sc.nextInt();
+        if (n%2 == 0) {
+            System.out.println(n);;
+        }
+        else {
+            System.out.println(2*n);;
+        }
+    }
 }
