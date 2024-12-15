3323ec25bbcaac457af66c5f43578f3200b0ff82
Laman400 <lmanquliyeva70@gmail.com>
2024-12-15 17:54:17 +0400

    movie app added

---
+++ src/lab1/Movie.java
@@ -0,0 +1,37 @@
+package lab1;
+
+public class Movie {
+
+    String name;
+
+    double rating;
+
+    public Movie(String name, double rating) {
+        this.name = name;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "name='" + name + '\'' +
+                ", rating=" + rating +
+                '}';
+    }
+}
